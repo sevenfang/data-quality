ddaa070e04c880407ee4c22fbe95d0c9a7783e87
qiongli <qiongli@talend.com>
2016-11-02 10:49:16 +0800

    TDQ-12474 load the survivorship rules from jar file for exporting osgi bunlder or TIC

--- main/plugins/org.talend.survivorship/src/org/talend/survivorship/SurvivorshipManager.java
+++ main/plugins/org.talend.survivorship/src/org/talend/survivorship/SurvivorshipManager.java
@@ -13,11 +13,13 @@
 package org.talend.survivorship;
 
 import java.io.File;
+import java.net.URL;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
 import org.kie.api.definition.type.FactType;
+import org.kie.api.io.Resource;
 import org.kie.api.io.ResourceType;
 import org.kie.internal.KnowledgeBase;
 import org.kie.internal.KnowledgeBaseFactory;
@@ -101,16 +103,25 @@ public class SurvivorshipManager extends KnowledgeManager {
     public void initKnowledgeBase() {
 
         int pos = rulePath.lastIndexOf('/');
+        boolean isClassPathResource = false;
         if (pos > 0 && pos < rulePath.length() + 1) {
             String projectName = rulePath.substring(pos + 1);
             if (projectName != null) {
                 String itemsFolder = "items/"; //$NON-NLS-1$
                 File f = new File(itemsFolder);
+                ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+                URL resource = contextClassLoader.getResource(itemsFolder);
                 if (f.exists()) { // running exported job
                     // since path is case sensitive in linux, call toLowerCase() here to correct the spelling.
                     rulePath = itemsFolder + projectName.toLowerCase();
                     f = new File(rulePath + "/metadata/survivorship/"); //$NON-NLS-1$
                     if (!f.exists()) {
+                        System.err.println("[INFO] This error may appear if you did not export the dependencies of the job."); //$NON-NLS-1$
+                    }
+                } else if (resource != null) {// running exported OSGI bundle or on TIC.the 'items' folder is in jar file.
+                    isClassPathResource = true;
+                    rulePath = resource.getFile().concat(projectName.toLowerCase());
+                    if (null == getClass().getResource(rulePath + "/metadata/survivorship/")) { //$NON-NLS-1$
                         System.err.println("[INFO] This error may appear if you did not export the dependencies of the job."); //$NON-NLS-1$
                     }
                 } else {
@@ -146,28 +157,28 @@ public class SurvivorshipManager extends KnowledgeManager {
 
         // add package declaration including declarative model
         kbuilder.add(
-                ResourceFactory.newFileResource(packagePath //$NON-NLS-1$ //$NON-NLS-2$
+                newResource(packagePath
                         + SurvivorshipConstants.DROOLS
                         + SurvivorshipConstants.VERSION_SUFFIX
-                        + SurvivorshipConstants.PKG_ITEM_EXTENSION), ResourceType.DRL);
+                        + SurvivorshipConstants.PKG_ITEM_EXTENSION, isClassPathResource), ResourceType.DRL);
 
         // add rule definitions
         for (RuleDefinition definition : ruleDefinitionList) {
             if (definition.getOrder().equals(Order.SEQ)) {
                 kbuilder.add(
-                        ResourceFactory.newFileResource(packagePath //$NON-NLS-1$ //$NON-NLS-2$
+                        newResource(packagePath
                                 + definition.getRuleName()
                                 + SurvivorshipConstants.VERSION_SUFFIX
-                                + SurvivorshipConstants.RULE_ITEM_EXTENSION), ResourceType.DRL);
+                                + SurvivorshipConstants.RULE_ITEM_EXTENSION, isClassPathResource), ResourceType.DRL);
             }
         }
 
         // add survivorship work flow
         kbuilder.add(
-                ResourceFactory.newFileResource(packagePath //$NON-NLS-1$ //$NON-NLS-2$
+                newResource(packagePath
                         + SurvivorshipConstants.SURVIVOR_FLOW
                         + SurvivorshipConstants.VERSION_SUFFIX
-                        + SurvivorshipConstants.FLOW_ITEM_EXTENSION), ResourceType.BPMN2);
+                        + SurvivorshipConstants.FLOW_ITEM_EXTENSION, isClassPathResource), ResourceType.BPMN2);
 
         KnowledgeBuilderErrors errors = kbuilder.getErrors();
         if (errors.size() > 0) {
@@ -180,6 +191,17 @@ public class SurvivorshipManager extends KnowledgeManager {
         kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
 
         dataset = new DataSet(columnList);
+    }
+
+    /**
+     * Support to create resource from jar file
+     */
+    public static Resource newResource(String packagePath, boolean isClassPathResource) {
+        if (isClassPathResource) {// the resource is in ClassPath,e.g, OSGI bundle or TIC
+            return ResourceFactory.newClassPathResource(packagePath);
+        } else {
+            return ResourceFactory.newFileResource(packagePath);
+        }
     }
 
     /**
