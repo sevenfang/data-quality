5539186d3c6b831d23b14838cbaf9ed8bff62b39
zshen-talend <zshen@talend.com>
2017-07-19 20:41:18 +0800

    bug fixed TDQ-13994 fixed null pointer exception for tRulesurvivorship (#159)
    Survivor As function

--- dataquality-survivorship/src/test/java/org/talend/survivorship/action/SurviveAsActionTest.java
+++ dataquality-survivorship/src/test/java/org/talend/survivorship/action/SurviveAsActionTest.java
@@ -29,16 +29,15 @@ public class SurviveAsActionTest {
 
     /**
      * Test method for
-     * {@link org.talend.survivorship.action.SurviveAsAction#canHandle(org.talend.survivorship.action.ActionParameter)}
-     * .
+     * {@link org.talend.survivorship.action.SurviveAsAction#canHandle(org.talend.survivorship.action.ActionParameter)} .
      */
     @Test
     public void testCheckCanHandle() {
 
-        Column col1 = new Column("city1", "String");
-        Column col2 = new Column("city2", "String");
-        Column col3 = new Column("city3", "String");
-        Column col4 = new Column("city4", "String");
+        Column col1 = new Column("city1", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+        Column col2 = new Column("city2", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+        Column col3 = new Column("city3", "String"); //$NON-NLS-1$ //$NON-NLS-2$
+        Column col4 = new Column("city4", "String"); //$NON-NLS-1$ //$NON-NLS-2$
         List<Column> columns = new ArrayList<>();
         columns.add(col1);
         columns.add(col2);
@@ -47,54 +46,78 @@ public class SurviveAsActionTest {
         DataSet dataset = new DataSet(columns);
         Record record1 = new Record();
         record1.setId(1);
-        Attribute att1 = new Attribute(record1, col1, "value1");
+        Attribute att1 = new Attribute(record1, col1, "value1"); //$NON-NLS-1$
         Attribute att2 = new Attribute(record1, col2, null);
-        Attribute att3 = new Attribute(record1, col2, "value33");
-        Attribute att4 = new Attribute(record1, col2, "value4");
-        record1.putAttribute("city1", att1);
-        record1.putAttribute("city2", att2);
-        record1.putAttribute("city3", att3);
-        record1.putAttribute("city4", att4);
+        Attribute att3 = new Attribute(record1, col2, "value33"); //$NON-NLS-1$
+        Attribute att4 = new Attribute(record1, col2, "value4"); //$NON-NLS-1$
+        record1.putAttribute("city1", att1); //$NON-NLS-1$
+        record1.putAttribute("city2", att2); //$NON-NLS-1$
+        record1.putAttribute("city3", att3); //$NON-NLS-1$
+        record1.putAttribute("city4", att4); //$NON-NLS-1$
         col1.putAttribute(record1, att1);
         col2.putAttribute(record1, att2);
         col3.putAttribute(record1, att3);
         col4.putAttribute(record1, att4);
         Record record2 = new Record();
         record2.setId(2);
-        att1 = new Attribute(record2, col1, "value11");
-        att2 = new Attribute(record2, col2, "value2");
-        att3 = new Attribute(record2, col2, "value3");
-        att4 = new Attribute(record2, col2, "value4");
-        record2.putAttribute("city1", att1);
-        record2.putAttribute("city2", att2);
-        record2.putAttribute("city3", att3);
-        record2.putAttribute("city4", att4);
+        att1 = new Attribute(record2, col1, "value11"); //$NON-NLS-1$
+        att2 = new Attribute(record2, col2, "value2"); //$NON-NLS-1$
+        att3 = new Attribute(record2, col2, "value3"); //$NON-NLS-1$
+        att4 = new Attribute(record2, col2, "value4"); //$NON-NLS-1$
+        record2.putAttribute("city1", att1); //$NON-NLS-1$
+        record2.putAttribute("city2", att2); //$NON-NLS-1$
+        record2.putAttribute("city3", att3); //$NON-NLS-1$
+        record2.putAttribute("city4", att4); //$NON-NLS-1$
         col1.putAttribute(record2, att1);
         col2.putAttribute(record2, att2);
         col3.putAttribute(record2, att3);
         col4.putAttribute(record2, att4);
         dataset.getRecordList().add(record1);
         dataset.getRecordList().add(record2);
-        dataset.getSurvivorIndexMap().put("city1", 1);
-        dataset.getSurvivorIndexMap().put("city2", 0);
-        dataset.getSurvivorIndexMap().put("city3", 1);
-        dataset.getSurvivorIndexMap().put("city4", 1);
+        dataset.getSurvivorIndexMap().put("city1", 1); //$NON-NLS-1$
+        dataset.getSurvivorIndexMap().put("city2", 0); //$NON-NLS-1$
+        dataset.getSurvivorIndexMap().put("city3", 1); //$NON-NLS-1$
+        dataset.getSurvivorIndexMap().put("city4", 1); //$NON-NLS-1$
         Object inputData = null;
         int rowNum = 0;
-        String column = "city2";
-        String ruleName = "rule1";
-        String expression = "Tony,Green";
+        String column = "city2"; //$NON-NLS-1$
+        String ruleName = "rule1"; //$NON-NLS-1$
+        String expression = "Tony,Green"; //$NON-NLS-1$
         boolean ignoreBlanks = false;
         ActionParameter actionParameter = new ActionParameter(dataset, inputData, rowNum, column, ruleName, expression,
                 ignoreBlanks);
         SurviveAsAction survivedValuePassAction = new SurviveAsAction();
         boolean checkCanHandle = survivedValuePassAction.canHandle(actionParameter);
-        Assert.assertTrue("intput data is same with survived value both null so that result should be true", checkCanHandle);
+        Assert.assertTrue("intput data is same with survived value both null so that result should be true", checkCanHandle); //$NON-NLS-1$
 
         inputData = 0;
         actionParameter = new ActionParameter(dataset, inputData, rowNum, column, ruleName, expression, ignoreBlanks);
         checkCanHandle = survivedValuePassAction.canHandle(actionParameter);
-        Assert.assertFalse("intput data is not same with survived value so that result should be false", checkCanHandle);
+        Assert.assertFalse("intput data is not same with survived value so that result should be false", checkCanHandle); //$NON-NLS-1$
+    }
+
+    /**
+     * Test method for
+     * {@link org.talend.survivorship.action.SurviveAsAction#canHandle(org.talend.survivorship.action.ActionParameter)} .
+     * case2 can not find sruvived value of refcolumn
+     */
+    @Test
+    public void testCheckCanHandleRefColumnNonResolvedConflict() {
+
+        List<Column> columns = new ArrayList<>();
+        DataSet dataset = new DataSet(columns);
+        Object inputData = null;
+        int rowNum = 0;
+        String column = "city2"; //$NON-NLS-1$
+        String ruleName = "rule1"; //$NON-NLS-1$
+        String expression = "Tony,Green"; //$NON-NLS-1$
+        boolean ignoreBlanks = false;
+        ActionParameter actionParameter = new ActionParameter(dataset, inputData, rowNum, column, ruleName, expression,
+                ignoreBlanks);
+        SurviveAsAction survivedValuePassAction = new SurviveAsAction();
+        boolean checkCanHandle = survivedValuePassAction.canHandle(actionParameter);
+        Assert.assertFalse("can not get survivorIndex for column city2 so that result should be false", checkCanHandle); //$NON-NLS-1$
+
     }
 
 }
