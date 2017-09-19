793cfda9b53c33676f028d6cb00e58805ede0372
scorreia <scorreia@talend.com>
2017-09-19 17:44:53 +0200

    chore(TDQ-14048) add some small performance tests on StringTrimmer (#212)
    * chore(TDQ-14048) add some small performance tests on StringTrimmer
    
    * chore(TDQ-14048): improve StringTrimmer

--- dataquality-converters/src/main/java/org/talend/dataquality/converters/StringTrimmer.java
+++ dataquality-converters/src/main/java/org/talend/dataquality/converters/StringTrimmer.java
@@ -12,47 +12,46 @@
 // ============================================================================
 package org.talend.dataquality.converters;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.commons.lang.StringUtils;
 
 /**
  * this class is used to remove trailing and leading characters.<br/>
  * created by msjian on 2017.2.16
- * 
+ *
  */
 public class StringTrimmer {
 
-    public String[] WHITESPACE_CHARS = new String[] { "\t" // CHARACTER TABULATION //$NON-NLS-1$
-            , "\n" // LINE FEED (LF) //$NON-NLS-1$
-            , '\u000B' + "" // LINE TABULATION //$NON-NLS-1$
-            , "\f" // FORM FEED (FF) //$NON-NLS-1$
-            , "\r" // CARRIAGE RETURN (CR) //$NON-NLS-1$
-            , " " // SPACE //$NON-NLS-1$
-            , '\u0085' + "" // NEXT LINE (NEL) //$NON-NLS-1$
-            , '\u00A0' + "" // NO-BREAK SPACE //$NON-NLS-1$
-            , '\u1680' + "" // OGHAM SPACE MARK //$NON-NLS-1$
-            , '\u180E' + "" // MONGOLIAN VOWEL SEPARATOR //$NON-NLS-1$
-            , '\u2000' + "" // EN QUAD //$NON-NLS-1$
-            , '\u2001' + "" // EM QUAD //$NON-NLS-1$
-            , '\u2002' + "" // EN SPACE //$NON-NLS-1$
-            , '\u2003' + "" // EM SPACE //$NON-NLS-1$
-            , '\u2004' + "" // THREE-PER-EM SPACE //$NON-NLS-1$
-            , '\u2005' + "" // FOUR-PER-EM SPACE //$NON-NLS-1$
-            , '\u2006' + "" // SIX-PER-EM SPACE //$NON-NLS-1$
-            , '\u2007' + "" // FIGURE SPACE //$NON-NLS-1$
-            , '\u2008' + "" // PUNCTUATION SPACE //$NON-NLS-1$
-            , '\u2009' + "" // THIN SPACE //$NON-NLS-1$
-            , '\u200A' + "" // HAIR SPACE //$NON-NLS-1$
-            , '\u2028' + "" // LINE SEPARATOR //$NON-NLS-1$
-            , '\u2029' + "" // PARAGRAPH SEPARATOR //$NON-NLS-1$
-            , '\u202F' + "" // NARROW NO-BREAK SPACE //$NON-NLS-1$
-            , '\u205F' + "" // MEDIUM MATHEMATICAL SPACE //$NON-NLS-1$
-            , '\u3000' + "" // IDEOGRAPHIC SPACE //$NON-NLS-1$
-    };
+    public static final Set<Character> WHITESPACE_CHARS_SUPERIOR_ASCII_SPACE = new HashSet<>(Arrays.asList( //
+            '\u0085' // NEXT LINE (NEL) //$NON-NLS-1$
+            , '\u00A0' // NO-BREAK SPACE //$NON-NLS-1$
+            , '\u1680' // OGHAM SPACE MARK //$NON-NLS-1$
+            , '\u180E' // MONGOLIAN VOWEL SEPARATOR //$NON-NLS-1$
+            , '\u2000' // EN QUAD //$NON-NLS-1$
+            , '\u2001' // EM QUAD //$NON-NLS-1$
+            , '\u2002' // EN SPACE //$NON-NLS-1$
+            , '\u2003' // EM SPACE //$NON-NLS-1$
+            , '\u2004' // THREE-PER-EM SPACE //$NON-NLS-1$
+            , '\u2005' // FOUR-PER-EM SPACE //$NON-NLS-1$
+            , '\u2006' // SIX-PER-EM SPACE //$NON-NLS-1$
+            , '\u2007' // FIGURE SPACE //$NON-NLS-1$
+            , '\u2008' // PUNCTUATION SPACE //$NON-NLS-1$
+            , '\u2009' // THIN SPACE //$NON-NLS-1$
+            , '\u200A' // HAIR SPACE //$NON-NLS-1$
+            , '\u2028' // LINE SEPARATOR //$NON-NLS-1$
+            , '\u2029' // PARAGRAPH SEPARATOR //$NON-NLS-1$
+            , '\u202F' // NARROW NO-BREAK SPACE //$NON-NLS-1$
+            , '\u205F' // MEDIUM MATHEMATICAL SPACE //$NON-NLS-1$
+            , '\u3000' // IDEOGRAPHIC SPACE //$NON-NLS-1$
+    ));
 
     /**
      * Remove trailing and leading characters which may be empty string, space string,\t,\n,\r,\f...any space, break related
      * characters.
-     * 
+     *
      * @param inputStr - the input text.
      * @return String
      */
@@ -61,15 +60,20 @@ public class StringTrimmer {
             return inputStr;
         }
 
-        String result = inputStr.trim();
-        while (StringUtils.startsWithAny(result, WHITESPACE_CHARS)) {
-            result = StringUtils.removeStart(result, result.substring(0, 1));
+        int startIndex = 0;
+        int endIndex = inputStr.length();
+        char currentCharacter = inputStr.charAt(startIndex);
+        while (startIndex < endIndex
+                && (currentCharacter <= ' ' || WHITESPACE_CHARS_SUPERIOR_ASCII_SPACE.contains(currentCharacter))) {
+            currentCharacter = inputStr.charAt(++startIndex);
         }
+        if (startIndex == endIndex)
+            return "";
+        do {
+            currentCharacter = inputStr.charAt(--endIndex);
+        } while (currentCharacter <= ' ' || WHITESPACE_CHARS_SUPERIOR_ASCII_SPACE.contains(currentCharacter));
 
-        while (StringUtils.endsWithAny(result, WHITESPACE_CHARS)) {
-            result = StringUtils.removeEnd(result, result.substring(result.length() - 1, result.length()));
-        }
-        return result;
+        return inputStr.substring(startIndex, endIndex + 1);
     }
 
     /**
@@ -78,7 +82,7 @@ public class StringTrimmer {
      * Note: this is not equals inputStr.trim() for example: <br/>
      * when the inputStr is ("\t" + "abc "), this method will return ("\t" + "abc"),<br/>
      * but for trim method will return "abc"
-     * 
+     *
      * @param inputStr - the input text.
      * @return String
      */
@@ -88,25 +92,54 @@ public class StringTrimmer {
 
     /**
      * Remove trailing and leading characters.
-     * 
+     *
      * @param inputStr - the input text.
-     * @param removeCharacter - the remove character.
+     * @param removeStr - the remove string.
      * @return String.
      */
-    public String removeTrailingAndLeading(String inputStr, String removeCharacter) {
-        if (StringUtils.isEmpty(inputStr) || StringUtils.isEmpty(removeCharacter)) {
+    public String removeTrailingAndLeading(String inputStr, String removeStr) {
+
+        if (StringUtils.isEmpty(inputStr) || StringUtils.isEmpty(removeStr)) {
             return inputStr;
+        } else if (removeStr.length() == 1) {
+            return removeTrailingAndLeading(inputStr, removeStr.charAt(0));
         }
 
         String result = inputStr;
 
-        while (result.startsWith(removeCharacter)) {
-            result = StringUtils.removeStart(result, removeCharacter);
+        while (result.startsWith(removeStr)) {
+            result = StringUtils.removeStart(result, removeStr);
         }
-        while (result.endsWith(removeCharacter)) {
-            result = StringUtils.removeEnd(result, removeCharacter);
+        while (result.endsWith(removeStr)) {
+            result = StringUtils.removeEnd(result, removeStr);
         }
         return result;
     }
 
+    /**
+     * Remove trailing and leading characters more quickly than removeTrailingAndLeading(String, String)
+     *
+     * @param inputStr
+     * @param removeCharacter
+     * @return
+     */
+    public String removeTrailingAndLeading(String inputStr, Character removeCharacter) {
+        if (StringUtils.isEmpty(inputStr)) {
+            return inputStr;
+        }
+
+        int startIndex = 0;
+        int endIndex = inputStr.length();
+        char currentCharacter = inputStr.charAt(startIndex);
+        while (startIndex < endIndex && (currentCharacter == removeCharacter)) {
+            currentCharacter = inputStr.charAt(++startIndex);
+        }
+        if (startIndex == endIndex)
+            return "";
+        do {
+            currentCharacter = inputStr.charAt(--endIndex);
+        } while (currentCharacter == removeCharacter);
+
+        return inputStr.substring(startIndex, endIndex + 1);
+    }
 }
--- dataquality-converters/src/test/java/org/talend/dataquality/converters/StringTrimmerTest.java
+++ dataquality-converters/src/test/java/org/talend/dataquality/converters/StringTrimmerTest.java
@@ -14,15 +14,50 @@ package org.talend.dataquality.converters;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Test for class {@link StringTrimmer}.
- * 
+ *
  * @author msjian
  * @version 2017.02.08
  */
 public class StringTrimmerTest {
+
+    public String[] WHITESPACE_CHARS = new String[] { "\t" // CHARACTER TABULATION //$NON-NLS-1$
+            , "\n" // LINE FEED (LF) //$NON-NLS-1$
+            , '\u000B' + "" // LINE TABULATION //$NON-NLS-1$
+            , "\f" // FORM FEED (FF) //$NON-NLS-1$
+            , "\r" // CARRIAGE RETURN (CR) //$NON-NLS-1$
+            , " " // SPACE //$NON-NLS-1$
+            , '\u0085' + "" // NEXT LINE (NEL) //$NON-NLS-1$
+            , '\u00A0' + "" // NO-BREAK SPACE //$NON-NLS-1$
+            , '\u1680' + "" // OGHAM SPACE MARK //$NON-NLS-1$
+            , '\u180E' + "" // MONGOLIAN VOWEL SEPARATOR //$NON-NLS-1$
+            , '\u2000' + "" // EN QUAD //$NON-NLS-1$
+            , '\u2001' + "" // EM QUAD //$NON-NLS-1$
+            , '\u2002' + "" // EN SPACE //$NON-NLS-1$
+            , '\u2003' + "" // EM SPACE //$NON-NLS-1$
+            , '\u2004' + "" // THREE-PER-EM SPACE //$NON-NLS-1$
+            , '\u2005' + "" // FOUR-PER-EM SPACE //$NON-NLS-1$
+            , '\u2006' + "" // SIX-PER-EM SPACE //$NON-NLS-1$
+            , '\u2007' + "" // FIGURE SPACE //$NON-NLS-1$
+            , '\u2008' + "" // PUNCTUATION SPACE //$NON-NLS-1$
+            , '\u2009' + "" // THIN SPACE //$NON-NLS-1$
+            , '\u200A' + "" // HAIR SPACE //$NON-NLS-1$
+            , '\u2028' + "" // LINE SEPARATOR //$NON-NLS-1$
+            , '\u2029' + "" // PARAGRAPH SEPARATOR //$NON-NLS-1$
+            , '\u202F' + "" // NARROW NO-BREAK SPACE //$NON-NLS-1$
+            , '\u205F' + "" // MEDIUM MATHEMATICAL SPACE //$NON-NLS-1$
+            , '\u3000' + "" // IDEOGRAPHIC SPACE //$NON-NLS-1$
+    };
 
     private static final String expected = "abc"; //$NON-NLS-1$
 
@@ -50,7 +85,7 @@ public class StringTrimmerTest {
         assertEquals("a b c", stringTrimmer.removeTrailingAndLeading(" a b c ")); //$NON-NLS-1$ //$NON-NLS-2$
 
         // test for other characters
-        assertEquals(expected, stringTrimmer.removeTrailingAndLeading("\t" + expected, "\t")); //$NON-NLS-1$ //$NON-NLS-2$ 
+        assertEquals(expected, stringTrimmer.removeTrailingAndLeading("\t" + expected, "\t")); //$NON-NLS-1$ //$NON-NLS-2$
         assertEquals(expected, stringTrimmer.removeTrailingAndLeading(expected + "\t", "\t")); //$NON-NLS-1$ //$NON-NLS-2$
         assertEquals(expected, stringTrimmer.removeTrailingAndLeading('\u0009' + expected, "\t")); //$NON-NLS-1$
         assertEquals(expected, stringTrimmer.removeTrailingAndLeading('\u0009' + expected, '\u0009' + "")); //$NON-NLS-1$
@@ -58,15 +93,16 @@ public class StringTrimmerTest {
 
         assertEquals("abc ", stringTrimmer.removeTrailingAndLeading("\t" + "abc ", "\t")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
         assertEquals("a" + "\t" + "bc", stringTrimmer.removeTrailingAndLeading("\t" + "a" + "\t" + "bc", "\t")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$
-        assertEquals("\t" + expected, stringTrimmer.removeTrailingAndLeading("\t" + "abc ")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
-        assertEquals(expected, ("\t" + "abc ").trim()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3
+        assertEquals("\t" + expected, stringTrimmer.removeTrailingAndLeading("\t" + "abc ")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        assertEquals(expected, ("\t" + "abc ").trim()); //$NON-NLS-1$ //$NON-NLS-2$
+        // //$NON-NLS-3
 
-        assertEquals(expected, stringTrimmer.removeTrailingAndLeading("\n" + expected, "\n")); //$NON-NLS-1$ //$NON-NLS-2$ 
+        assertEquals(expected, stringTrimmer.removeTrailingAndLeading("\n" + expected, "\n")); //$NON-NLS-1$ //$NON-NLS-2$
         assertEquals("abc ", stringTrimmer.removeTrailingAndLeading("\n" + "abc ", "\n")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 
         assertEquals(expected, stringTrimmer.removeTrailingAndLeading(expected, "\r")); //$NON-NLS-1$
-        assertEquals(expected, stringTrimmer.removeTrailingAndLeading("\r" + expected, "\r")); //$NON-NLS-1$ //$NON-NLS-2$ 
-        assertEquals(expected, stringTrimmer.removeTrailingAndLeading("\r" + expected + "\r", "\r")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
+        assertEquals(expected, stringTrimmer.removeTrailingAndLeading("\r" + expected, "\r")); //$NON-NLS-1$ //$NON-NLS-2$
+        assertEquals(expected, stringTrimmer.removeTrailingAndLeading("\r" + expected + "\r", "\r")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         assertEquals("abc ", stringTrimmer.removeTrailingAndLeading("\r" + "abc ", "\r")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
         assertEquals("abc ", stringTrimmer.removeTrailingAndLeading("\r" + "abc " + "\r", "\r")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 
@@ -79,10 +115,86 @@ public class StringTrimmerTest {
     public void testRemoveTrailingAndLeadingWhitespaces() {
         StringTrimmer stringTrimmer = new StringTrimmer();
         String inputData = " " + expected; //$NON-NLS-1$
-        for (String removechar : stringTrimmer.WHITESPACE_CHARS) {
+        for (String removechar : WHITESPACE_CHARS) {
             inputData = inputData + removechar;
         }
         assertEquals(expected, stringTrimmer.removeTrailingAndLeadingWhitespaces(inputData));
     }
 
+    @Test
+    public void testRemoveTrailingAndLeadingWhitespacesOptimization() {
+        int nbTests = 10000;
+        for (int maxNbSpace = 1; maxNbSpace < 10; maxNbSpace += 3) {
+            System.out.print("Test with max Nb Space = " + maxNbSpace + "\t");
+            String[] ex = generateExamples(nbTests, maxNbSpace);
+            for (int i = 0; i < 2; i++) { // first run is taking a long time =>
+                                              // run twice at least
+                runPerfTests(ex, i);
+            }
+        }
+
+    }
+
+    private void runPerfTests(String[] ex, int runNumber) {
+        // call string trimmer
+        StringTrimmer stringTrimmer = new StringTrimmer();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < ex.length; i++) {
+            stringTrimmer.removeTrailingAndLeadingWhitespaces(ex[i]);
+        }
+        long duration1 = System.currentTimeMillis() - start;
+        if (runNumber == 1) // only display second run results (first run is
+                                // taking too much time to start
+            System.out.print("New trimmer implementation: " + duration1 + "\t");
+
+        // call original implementation
+        start = System.currentTimeMillis();
+        for (int i = 0; i < ex.length; i++) {
+            originalRemoveTrailingAndLeadingWhitespaces(ex[i]);
+        }
+        long duration2 = System.currentTimeMillis() - start;
+        if (runNumber == 1) {
+            System.out.println("Original trimmer implementation: " + duration2);
+            Assert.assertTrue("New StringTrimmer implementation should be faster than original implemenation. Run: " + runNumber,
+                    duration1 <= (duration2 + 10));
+        }
+    }
+
+    private String[] generateExamples(int n, int maxNbSpace) {
+        List<String> examples = new ArrayList<String>();
+        Random rng = new Random();
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder base = new StringBuilder("mdjqfdsqjpoierupoezflmdjdfsdvxcsdhnnkqfklm");
+            for (int j = 0; j < maxNbSpace; j++) {
+                // prepend spaces before base string
+                char[] chars = new char[rng.nextInt(maxNbSpace)];
+                Arrays.fill(chars, ' ');
+                base.insert(0, chars);
+                // append spaces after base string
+                chars = new char[rng.nextInt(maxNbSpace)];
+                Arrays.fill(chars, ' ');
+                base.append(chars);
+            }
+            examples.add(base.toString());
+        }
+        return examples.toArray(new String[examples.size()]);
+    }
+
+    private String originalRemoveTrailingAndLeadingWhitespaces(String inputStr) {
+        if (StringUtils.isEmpty(inputStr)) {
+            return inputStr;
+        }
+
+        String result = inputStr; // original
+        // String result = inputStr.trim(); // pull request from davkrause
+        while (StringUtils.startsWithAny(result, WHITESPACE_CHARS)) {
+            result = StringUtils.removeStart(result, result.substring(0, 1));
+        }
+
+        while (StringUtils.endsWithAny(result, WHITESPACE_CHARS)) {
+            result = StringUtils.removeEnd(result, result.substring(result.length() - 1, result.length()));
+        }
+        return result;
+    }
 }
