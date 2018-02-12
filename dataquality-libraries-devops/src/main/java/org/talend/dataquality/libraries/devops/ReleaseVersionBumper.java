2b37930819e270ec712b0af9f09c7e8770051b63
build-talend <build-talend@users.noreply.github.com>
2018-02-12 18:24:36 +0200

    chore(Jenkins): bump to next dev version 5.0.1-SNAPSHOT (#318)

--- dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
+++ dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
@@ -56,7 +56,7 @@ import org.xml.sax.SAXException;
  */
 public class ReleaseVersionBumper {
 
-    private static final String TARGET_VERSION = "5.0.0";
+    private static final String TARGET_VERSION = "5.0.1-SNAPSHOT";
 
     private static final String DATAQUALITY_PREFIX = "dataquality.";
 
