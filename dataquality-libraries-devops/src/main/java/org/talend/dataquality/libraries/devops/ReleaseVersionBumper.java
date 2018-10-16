0731e97250841aaa9d08e5dafb7008305dc67e5d
build-talend <build-talend@users.noreply.github.com>
2018-10-16 14:06:41 +0300

    chore(Jenkins): Bump to next dev version 6.1.2-SNAPSHOT (#469)

--- dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
+++ dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/ReleaseVersionBumper.java
@@ -56,9 +56,9 @@ import org.xml.sax.SAXException;
  */
 public class ReleaseVersionBumper {
 
-    private static final String TARGET_VERSION = "6.1.1";
+    private static final String TARGET_VERSION = "6.1.2-SNAPSHOT";
 
-    private static final String TARGET_DAIKON_VERSION = "0.26.1";
+    private static final String TARGET_DAIKON_VERSION = "";
 
     private static final String DAIKON_VERSION_PROPERTY_NAME = "org.talend.daikon.version";
 
--- dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/UpdateComponentDefinition.java
+++ dataquality-libraries-devops/src/main/java/org/talend/dataquality/libraries/devops/UpdateComponentDefinition.java
@@ -47,9 +47,9 @@ public class UpdateComponentDefinition {
 
     private static final String COMPONENTS_FOLDER = "/components";
 
-    private static final String DQ_LIB_VERSION = "6.1.1-SNAPSHOT";
+    private static final String DQ_LIB_VERSION = "6.1.2-SNAPSHOT";
 
-    private static final String DAIKON_VERSION = "0.26.1-SNAPSHOT";
+    private static final String DAIKON_VERSION = "";
 
     private static final String[] PROVIDERS = new String[] { //
             "/org.talend.designer.components.tdqprovider", //
